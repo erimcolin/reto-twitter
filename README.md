479fbdb44432b1fe5e9c4e1503ad3cbb8181b544
erimcolin <gabymcolin@gmail.com>
2017-12-05 17:41:33 -0600

    Twitter version 0.0.6

---
+++ README.md
@@ -0,0 +1,83 @@
+# Twitter
+
+* **Curso:** _Haciendo tu sitio web interactivo_
+* **Unidad:** _Creando interaccion con Javascrip_
+
+***
+## Objetivo
+
+Replicar el newsfeed de **Twitter** con diferentes versiones.
+
+***
+
+Para completar este reto trabajamos en colaboracion por squad para crear una replica de la web "Twitter" donde a travès de funciones creadas en DOM, podamos emitir un "tweet". El reto debe quedar de la siguiente manera:
+
+![Twitter Website](assets/images/giphy.gif)
+
+## Flujo de trabajo
+
+1. Crear el maquetado de las carpetas
+ 1.0 Assets
+  1.1 Images (guardaremos todas las imagenes que usaremos en nuestra web)
+ 1.2 CSS
+  1.1 css (donde guardaremos nuestro estilo de pagina web)
+ 1.3 JS
+  1.1 js (guarda nuestro codigo escrito en Javascrip)
+ 1.4 index.html (guarda nuestro codigo escrito en html)
+ 1.5 README.md (flujo de nuestra pagina web)
+
+2. Crear un repositorio local.
+
+3. Subir nuestro respositorio local a GitHub
+
+4. Crear diagrama flujo.
+
+5. Maquetar, dar funciones y estilo a nuestra pagina web con diferentes versiones.
+
+
+## Consideraciones
+```
+* Version 0.0.1
+```
+- Diseñar formulario para ingresar texto y el boton que permita "twittear"
+- Agregar evento "click" al boton al formulario
+- Obtener el texto del evento
+- Agregar el texto a nuestro HTML
+
+```
+* Version 0.0.2
+```
+- Si el usuario no ingresa texto deshabilitar el boton "twittear".
+- Ir contando los caracteres de forma regresiva.
+
+```
+* Version 0.0.3
+```
+
+- Si el tweet rebasa los 140 caracteres, deshabilitar el boton.
+- Si rebasa los 120 caracteres, mostrar el contador de color amarillo
+- Si rebasa los 130 caracteres, mostrar el contador de color rojo.
+- Si rebasa de los 140 caracteres, mostrar el contador en negativo y color negro.
+
+```
+* Version 0.0.4
+```
+
+- Al presionar enter debera crecer el texarea de acuerdo al texto ingresado por el usuario
+
+```
+* Version 0.0.5 (Extra)
+```
+
+- Si la cantidad de caracteres es mayor al textarea, debe agregarse una línea más debajo para no aparecer la barra de scroll.
+
+```
+* Version 0.0.6 (Extra)
+```
+- Agregar la hora en la que se publico el tweet. Formato en 24 hrs.
+
+> Nota: No detenerse demasiado tiempo en el diseño de la pagina, es decir, fuentes y colores.
+
+##Diagrama de flujo
+
+![Diagrama de Flujo](assets/images/diagrama.png)
---
+++ js/app.js
@@ -0,0 +1,76 @@
+//se mandan traer los elementos del HTML al JS
+var boton = document.getElementById("sent-text");
+var text = document.getElementById("text");
+var newParraf = document.getElementById("container");
+var letterCount = document.getElementById("characteres");
+//version 0.0.1
+//se agrega el evento click en el boton de tweet para que imprima los mensajes
+boton.addEventListener("click", twittear)
+
+function twittear(event) {
+   if (text.value) {
+     var parraf = document.createElement("p");
+     var divContainer = document.createElement("div");
+     parraf.textContent = text.value;
+     divContainer.appendChild(parraf);
+     newParraf.appendChild(divContainer);
+     divContainer.classList.add("content");
+     texto.value = "";
+     parraf.style.marginLeft = '14px';
+   }
+};
+// version 0.0.2
+//se agrega un contador que lleva la cuenta regresiva
+text.addEventListener("keyup", countCharacters);
+
+function countCharacters(event){
+  var count = 0;
+  // var letter = text.value.length;
+  count = text.value.length;
+  letterCount.value = 140 - count;
+/*
+version 0.0.3
+1.-Si pasa los 140 caracteres se desabilita el boton y cambia de color
+2. si para los 120 caracteres, mostrar el boton en otro color
+... etc */
+    if ( count > 140) {
+        boton.disabled = true;
+        letterCount.style.color = "black";
+        boton.style.color = 'grey';
+        boton.style.background = 'lightgrey';
+    } else if (count >  119 && count < 130) {
+        letterCount.style.color = "orange";
+    } else if (count > 129 && count < 140) {
+        letterCount.style.color = "red";
+    } else {
+        boton.disabled = false;
+        letterCount.style.color = "blue";
+        boton.style.color = 'white';
+        boton.style.background = '#6ad2f3'
+    }
+};
+/*version 0.0.4
+al presionar enter el textarea crece de acuerdo al tamaño del texto */
+var textarea = document.querySelector('textarea');
+textarea.addEventListener('keydown', autosize);
+
+function autosize(){
+  var el = this;
+  setTimeout(function(){
+    el.style.cssText = 'height:auto; padding:0';
+    el.style.cssText = 'height:' + el.scrollHeight + 'px';
+  }, 0);
+};
+// agregar la hora
+// var tiempo = moment("2016-01-01").format("MMM Do YY");
+// console.log(tiempo);
+boton.addEventListener("click", showHour)
+function showHour(event){
+  momentoActual = new Date();
+  hora = momentoActual.getHours();
+  minuto = momentoActual.getMinutes();
+  segundo = momentoActual.getSeconds();
+
+  printHour = hora + ' : ' + minuto + ' : ' + segundo;
+  document.getElementById('showHour').innerHTML = printHour;
+};
